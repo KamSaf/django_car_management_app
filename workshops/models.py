34bf0a9934c1d8b3d7308256d1334768261ab6d5
KamSaf <bopaf42@gmail.com>
2024-02-23 15:06:09 +0100

    Created workshop database model

--- workshops/models.py
+++ workshops/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Workshop(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.TextField(blank=False, max_length=100)
+    city = models.TextField(blank=True, max_length=100)
+    street = models.TextField(blank=True, max_length=100)
+    phone_number = models.TextField(blank=True, max_length=9)
+    profession = models.TextField(blank=True, max_length=100)
+    create_date = models.DateTimeField(default=timezone.now)
+    favourite = models.BooleanField(default=False, blank=False)
