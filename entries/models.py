24f9a012b59563003232118b2b69bbd3410e8d85
KamSaf <bopaf42@gmail.com>
2024-03-07 18:11:29 +0100

    Added Entry database model

--- entries/models.py
+++ entries/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cars.models import Car
+from django.utils import timezone
 
-# Create your models here.
+
+class Entry(models.Model):
+
+    TYPES_OF_ENTRIES = {
+        "service": "Service",
+        "fuel": "Fuel",
+        "others": "Others",
+    }
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    cost = models.FloatField(default=0)
+    category = models.TextField(blank=False, choices=TYPES_OF_ENTRIES)
+    date = models.DateTimeField(blank=False)
+    notes = models.TextField(blank=True, max_length=255)
+    create_date = models.DateTimeField(default=timezone.now)
+    last_edit_date = models.DateTimeField(default=timezone.now)
