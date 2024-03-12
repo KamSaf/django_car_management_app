0cfcadd068ef21312336c3e461b4de2d7600a251
KamSaf <bopaf42@gmail.com>
2024-03-12 16:33:16 +0100

    Created Reminder database model

--- reminders/models.py
+++ reminders/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cars.models import Car
+from django.utils import timezone
+from entries.models import Entry
 
-# Create your models here.
+
+class Reminder(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    category = models.TextField(blank=False, choices=Entry.TYPES_OF_ENTRIES)
+    date = models.DateTimeField(blank=False)
+    place = models.TextField(blank=True, max_length=200)
+    details = models.TextField(blank=True, max_length=100)
+    create_date = models.DateTimeField(default=timezone.now)
+    last_edit_date = models.DateTimeField(default=timezone.now)
