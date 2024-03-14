81187dccf1bae964e2913756a708289555ec3d3e
KamSaf <bopaf42@gmail.com>
2024-03-14 21:00:38 +0100

    Added exploitation reports

--- car_management_app/utils.py
+++ car_management_app/utils.py
@@ -1,5 +1,8 @@
 from cars.models import Car
 from django.contrib.auth.models import User
+from entries.models import Entry
+import calendar
+from django.utils import timezone
 
 
 def get_viewed_car(user: User, car_id: int = None) -> Car | None:
@@ -18,3 +21,51 @@ def get_viewed_car(user: User, car_id: int = None) -> Car | None:
             viewed_car = cars[0]
 
     return viewed_car
+
+
+def costs_sum(query, category: str = None) -> float:
+    """
+        Calculate sum of costs of given entries
+    """
+    if category:
+        query = query.filter(category=category)
+    return sum(query.values_list('cost', flat=True))
+
+
+def calc_report(car: Car, year: int, month: int) -> dict:
+    """
+        Calculate exploitation data for given month
+    """
+    month_days = timezone.now().day if timezone.now().month == month and timezone.now().year == year else calendar.monthrange(year, month)[1]
+
+    entries = Entry.objects.filter(car=car, date__year=year, date__month=month).order_by('date')
+    # spalanie zrobie po zrobieniu web scrappera z autocentrum
+    all_costs = costs_sum(query=entries)
+    fuel_costs = costs_sum(query=entries, category='fuel')
+    service_costs = costs_sum(query=entries, category='service')
+    others_costs = costs_sum(query=entries, category='others')
+    mileage_covered = entries.last().mileage - entries.first().mileage if len(entries) > 0 else 0
+
+    report = {
+        'all_costs': all_costs,
+        'fuel_costs': round((fuel_costs / all_costs) * 100, 1) if all_costs > 0 else 0,
+        'service_costs': round((service_costs / all_costs) * 100, 1) if all_costs > 0 else 0,
+        'others_costs': round((others_costs / all_costs) * 100, 1) if all_costs > 0 else 0,
+        'cost_per_day': round((all_costs / month_days), 2),
+        'cost_per_km': round(fuel_costs / mileage_covered, 2) if mileage_covered > 0 else 0,
+        'fuel_economy': 9.3,
+    }
+    return report
+
+
+def expl_report(car: Car, year: int, month: int) -> dict:
+    """
+        Create exploitation report for this and last months
+    """
+    this_month_report = calc_report(car=car, year=year, month=month)
+    if month > 1:
+        month -= 1
+    else:
+        month, year = 12, year - 1
+    last_month_report = calc_report(car=car, year=year, month=month)
+    return this_month_report, last_month_report
--- car_management_app/views.py
+++ car_management_app/views.py
@@ -7,7 +7,7 @@ from entries.forms import EntryForm
 from entries.models import Entry
 from reminders.models import Reminder
 from reminders.forms import ReminderForm
-from .utils import get_viewed_car
+from .utils import get_viewed_car, expl_report
 from django.utils import timezone
 
 
@@ -45,12 +45,11 @@ def home(request, car_id=None):
         return redirect(to='welcome_page')
 
     cars = Car.objects.filter(user=request.user).order_by('create_date').all()
-
     if len(cars) == 0:
         return redirect(to='add_new_car', first='true')
 
     workshops = Workshop.objects.filter(user=request.user).order_by('create_date').all()
-    favourite_workshops = Workshop.objects.filter(user=request.user, favourite=True).order_by('-last_edit_date').all()
+    favourite_workshops = workshops.filter(favourite=True).order_by('id', '-last_edit_date')
 
     viewed_car = get_viewed_car(user=request.user, car_id=car_id)
     entries = Entry.objects.filter(user=request.user, car=viewed_car).order_by('-date', '-create_date').all()
@@ -59,24 +58,17 @@ def home(request, car_id=None):
     viewed_car_mileage = last_entry.mileage if last_entry else 0
 
     reminders = Reminder.objects.filter(car=viewed_car, date__gte=timezone.now()).order_by('date').all()
-
-    # create forms
-    new_workshop_form = WorkshopForm(logged_user=request.user)
-    new_car_form = CarForm(logged_user=request.user)
-    new_entry_form = EntryForm(logged_user=request.user, car=viewed_car)
-    new_reminder_form = ReminderForm(logged_user=request.user, car=viewed_car)
-    edit_car_form = CarForm(instance=viewed_car, logged_user=request.user)
-    edit_car_form.set_initial(car=viewed_car)
+    this_month_report, last_month_report = expl_report(car=viewed_car, year=timezone.now().year, month=timezone.now().month)
 
     return render(
         request=request,
         template_name='home.html',
         context={
-            'new_workshop_form': new_workshop_form,
-            'new_car_form': new_car_form,
-            'new_entry_form': new_entry_form,
-            'edit_car_form': edit_car_form,
-            'new_reminder_form': new_reminder_form,
+            'new_workshop_form': WorkshopForm(logged_user=request.user),
+            'new_car_form': CarForm(logged_user=request.user),
+            'new_entry_form': EntryForm(logged_user=request.user, car=viewed_car),
+            'new_reminder_form': ReminderForm(logged_user=request.user, car=viewed_car),
+            'edit_car_form': CarForm(instance=viewed_car, logged_user=request.user).set_initial(car=viewed_car),
             'cars': cars,
             'viewed_car': viewed_car,
             'viewed_car_mileage': viewed_car_mileage,
@@ -84,5 +76,7 @@ def home(request, car_id=None):
             'favourite_workshops': favourite_workshops,
             'entries': entries,
             'reminders': reminders,
+            'this_month_data': this_month_report,
+            'last_month_data': last_month_report,
         }
     )
