ddfdf89c495e2ec934ebb47af01d615b1dbcad95
KamSaf <bopaf42@gmail.com>
2024-03-18 15:16:57 +0100

    Added average fuel consumption do a fuel economy graph

--- graphs/utils.py
+++ graphs/utils.py
@@ -86,31 +86,65 @@ def fuel_economy_graph(this_car_entries: list[Entry], other_cars_entries: list[E
     fuel_queryset = this_car_entries.filter(category='fuel').values('date', 'fuel_liters').order_by('date')
     mileage_queryset = this_car_entries.values('date', 'mileage').order_by('date')
 
+    this_car_df = fuel_cons(fuel_queryset=fuel_queryset, mileage_queryset=mileage_queryset)
+
+    cars = other_cars_entries.distinct('car').values_list('car', flat=True)
+    entries_by_cars = {}
+    for car_id in cars:
+        entries_by_cars[car_id] = (
+            other_cars_entries.filter(car__id=car_id, category='fuel').values('date', 'fuel_liters').order_by('date'),
+            other_cars_entries.filter(car__id=car_id).values('date', 'mileage').order_by('date')
+        )
+
+    avg_data = {month: 0 for month in this_car_df['month']}
+    for value in entries_by_cars.values():
+        df = fuel_cons(fuel_queryset=value[0], mileage_queryset=value[1], months_list=this_car_df['month'])
+        for row in df.iterrows():
+            avg_data[row[1].iloc[0]] += (row[1].iloc[1]) / len(entries_by_cars.values())
+
+    avg_df = pd.DataFrame({
+        'month': avg_data.keys(),
+        'expenses': avg_data.values(),
+    })
+
+    fig = go.Figure().add_trace(go.Scatter(x=this_car_df['month'], y=this_car_df['expenses'], mode='lines', name=f'Your {car.make} {car.model}'))
+    fig.add_trace(go.Scatter(x=avg_df['month'], y=avg_df['expenses'], mode='lines', name=f'Average for {car.make} {car.model}'))
+    fig.update_layout(autosize=True, title='Fuel economy:', xaxis_title='Month', yaxis_title='Fuel [l/km]', )
+    return fig.to_html()
+
+
+def fuel_cons(fuel_queryset, mileage_queryset, months_list: list = None) -> pd.DataFrame:
+    """
+        Creates DataFrame with fuel consumption data for given fuel and mileage querysets of a car
+    """
     if len(fuel_queryset) > 1 and len(mileage_queryset) > 1:
         data_fuel = [{list(dict.values())[0].strftime('%m/%Y'): list(dict.values())[1]} for dict in fuel_queryset]
         data_mileage = [{list(mileage_queryset[i].values())[0].strftime('%m/%Y'): (list(mileage_queryset[i].values())[1] - list(mileage_queryset[i - 1].values())[1])} for i in range(1, len(mileage_queryset))]
 
-        grouped_data_fuel = {}
-        for dict in data_fuel:
-            for key, value in dict.items():
-                grouped_data_fuel.setdefault(key, 0)
-                grouped_data_fuel[key] += value
-
-        grouped_data_mileage = {}
-        for dict in data_mileage:
-            for key, value in dict.items():
-                grouped_data_mileage.setdefault(key, 0)
-                grouped_data_mileage[key] += value
+        grouped_data_fuel = group_data(data=data_fuel)
+        grouped_data_mileage = group_data(data=data_mileage)
 
         data = {}
         for date, mileage in grouped_data_mileage.items():
             data[date] = (grouped_data_fuel[date] * 100) / mileage if date in grouped_data_fuel.keys() else 0
 
-        df = pd.DataFrame({
-            'month': data.keys(),
-            'expenses': data.values(),
-        })
+    this_car_df = pd.DataFrame({
+        'month': data.keys() if data else [],
+        'expenses': data.values() if data else [],
+    })
 
-        fig = go.Figure().add_trace(go.Scatter(x=df['month'], y=df['expenses'], mode='lines', name=f'Your {car.make} {car.model}'))
-        fig.update_layout(autosize=True, title='Fuel economy:', xaxis_title='Month', yaxis_title='Fuel [l/km]', )
-        return fig.to_html()
+    if months_list is not None:
+        this_car_df = this_car_df[this_car_df['month'].isin(months_list)]
+    return this_car_df
+
+
+def group_data(data: list[dict]) -> dict:
+    """
+        Groups list of dictionaries into single dictionary
+    """
+    grouped_data = {}
+    for dict in data:
+        for key, value in dict.items():
+            grouped_data.setdefault(key, 0)
+            grouped_data[key] += value
+    return grouped_data
