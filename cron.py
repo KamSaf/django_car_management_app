59d848740ebab91147c9200d4046c70ac3755636
KamSaf <bopaf42@gmail.com>
2025-06-16 23:42:12 +0200

    add static.json

--- cron.py
+++ cron.py
@@ -1,10 +1,10 @@
 import os
 import django
+from cronjob.utils import save_prices, get_fuel_prices
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "car_management_app.settings")
 django.setup()
 
 
 def update_fuel_prices():
-    from cronjob.utils import save_prices, get_fuel_prices
     save_prices(prices=get_fuel_prices())
